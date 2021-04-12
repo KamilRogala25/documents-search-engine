09cab62eaddc33c56794e78a0adfdc32214fcd9a
Kamil Rogala <rogalak25@gmail.com>
2021-04-12 13:51:00 +0200

    Empty project - ready to go

---
+++ src/main/java/com/example/documentssearchengine/DocumentsSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package com.example.documentssearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentsSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentsSearchEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/documentssearchengine/DocumentsSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.documentssearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentsSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
